9d490671497ced334fa39be512008c2e5bce19f3
bungamungil <bungamungil@icloud.com>
2023-11-14 20:40:16 +0700

    Update scroll sub district

--- resources/views/shipping-address/create.blade.php
+++ resources/views/shipping-address/create.blade.php
@@ -30,7 +30,7 @@
                                 <label for="subDistrict" class="block text-gray-700 text-sm font-bold mb-2">Sub District</label>
                                 <input type="text" id="subDistrict" name="subDistrict" class="w-full border rounded p-2" x-model="subDistrictQuery" @input.debounce.500="updateSubDistrictSuggestion" @focus="subDistrictSuggestionActive=true">
                                 <div class="relative" x-show="subDistrictSuggestions.length > 0 && subDistrictSuggestionActive" x-cloak x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 transform scale-y-90" x-transition:enter-end="opacity-100 transform scale-y-100" x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100 transform scale-y-100" x-transition:leave-end="opacity-0 transform scale-y-90">
-                                    <div class="absolute top-100 mt-1 w-full border bg-white shadow-xl rounded-xl">
+                                    <div class="absolute overflow-y-scroll h-auto max-h-96 top-100 mt-1 w-full border bg-white shadow-xl rounded-xl">
                                         <div class="p-3">
                                             <div class="" x-ref="list">
                                                 <template x-for="(suggestion, index) in subDistrictSuggestions" :key="index">
@@ -98,7 +98,7 @@
                     if (this.subDistrictQuery === '') {
                         return;
                     }
-                    fetch('/api/sub-district/search?query=' + this.subDistrictQuery + '&size=' + 8)
+                    fetch('/api/sub-district/search?query=' + this.subDistrictQuery + '&size=' + 80)
                         .then(response => response.json())
                         .then(data => {
                             console.log(data.map(item => item.name));
