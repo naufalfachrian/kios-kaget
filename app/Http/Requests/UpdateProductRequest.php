38f095d59d74bb2e8dba7ca11e0f6d754481e293
bungamungil <bungamungil@icloud.com>
2023-11-19 21:20:43 +0700

    Initial implementation for Product

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Permission;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user()->hasPermission(Permission::$PRODUCT_MASTER);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'price' => 'required|numeric|min:0',
+            'description' => 'required|string',
+            'weight_in_grams' => 'required|numeric|min:0',
+        ];
+    }
+}
