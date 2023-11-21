e0524b6404dcf39c743443a6df7abf492f825874
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-11-21 14:41:44 +0700

    Add administrator panel

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -134,5 +134,27 @@
                 </form>
             </div>
         </div>
+        @if (Auth::user()->hasPermission(\App\Models\Permission::$ADMINISTRATOR_ACCESS))
+            <div class="pt-4 pb-3 space-y-1 border-t border-gray-200 dark:border-gray-600">
+                <div class="inline-flex px-4">
+                    <div class="me-1 flex">
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                            </svg>
+                        </div>
+                    </div>
+
+                    {{ __('Administrator Panel') }}
+                </div>
+                @if (Auth::user()->hasPermission(\App\Models\Permission::$PRODUCT_MASTER))
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('products.index')">
+                        {{ __('Setup Product') }}
+                    </x-responsive-nav-link>
+                </div>
+                @endif
+            </div>
+        @endif
     </div>
 </nav>
