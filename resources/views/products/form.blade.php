dde9ad9d575d1208129f8d00d9eaa8dd4edbbc4f
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-11-21 15:27:53 +0700

    Update product image form

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -5,11 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12" x-data="productForm()">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form class="mx-auto bg-white rounded p-6" x-data="productForm()"
+                    <form class="mx-auto bg-white rounded p-6"
                           action="{{ isset($product) ? route('products.update', ['products' => $product->id]) : route('products.store') }}"
                           method="post">
                         @if(isset($product))
@@ -18,7 +18,9 @@
                         @csrf
                         <h3 class="block text-gray-700 text-sm font-bold mb-2">Product Image</h3>
                         <div class="grid md:grid-cols-4 grid-cols-2 gap-4 mb-4">
-                            <div class="bg-blue-50 aspect-square rounded-lg border-gray-300 border-2 flex items-center justify-center">
+                            <div class="bg-blue-50 aspect-square rounded-lg border-gray-300 border-2 flex items-center justify-center hover:cursor-pointer"
+                                 x-on:click.prevent="$dispatch('open-modal', 'product-image-form');"
+                            >
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                 </svg>
@@ -62,12 +64,65 @@
                 </div>
             </div>
         </div>
+        <x-modal name="product-image-form" focusable>
+            <form method="post" x-ref="form" class="p-6">
+                @csrf
+
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
+                    {{ __('Upload Product Images') }}
+                </h2>
+
+                <div class="lg:grid lg:grid-cols-2 flex flex-col gap-3">
+
+                    <img class="w-full aspect-square border rounded-lg auto" :src="inputProductImageSource" />
+
+                    <div class="flex flex-col grow">
+                        <div class="mb-4">
+                            <label for="image_name" class="block text-gray-700 text-sm font-bold mb-2">Image Label *</label>
+                            <input type="text" id="image_name" name="image_name" class="w-full border rounded p-2">
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="image_file" class="block text-gray-700 text-sm font-bold mb-2">Upload Image *</label>
+                            <input type="file" accept="image/jpeg" id="image_file" name="image_file" x-ref="image_file" class="w-full border rounded p-2" x-on:change="reloadPreviewProductImage()">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="mt-6 flex gap-3">
+                    <x-primary-button x-on:click="$dispatch('close')">
+                        {{ __('Save') }}
+                    </x-primary-button>
+
+                    <x-secondary-button class="ms-auto" x-on:click="$dispatch('close')">
+                        {{ __('Cancel') }}
+                    </x-secondary-button>
+
+                    <x-danger-button>
+                        {{ __('Delete') }}
+                    </x-danger-button>
+                </div>
+            </form>
+        </x-modal>
     </div>
 
     <script>
         function productForm() {
             return {
-
+                inputProductImageSource: null,
+                reloadPreviewProductImage() {
+                    console.log('reload-preview-product-image-called')
+                    let file = this.$refs.image_file.files[0];
+                    console.log('file received')
+                    if(!file || file.type.indexOf('image/') === -1) return;
+                    this.inputProductImageSource = null;
+                    let reader = new FileReader();
+                    reader.onload = e => {
+                        this.inputProductImageSource = e.target.result;
+                    }
+                    reader.readAsDataURL(file);
+                }
             }
         }
     </script>
