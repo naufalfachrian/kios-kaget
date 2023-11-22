5cf662517b4bfa878cb893bb5461fb5c58288d69
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-11-22 13:06:45 +0700

    Polished upload product image form

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -65,7 +65,7 @@
             </div>
         </div>
         <x-modal name="product-image-form" focusable>
-            <form method="post" x-ref="form" class="p-6" x-on:submit.prevent="submitProductImage()">
+            <form method="post" x-ref="productImageForm" class="p-6" x-on:submit.prevent="submitProductImage()">
                 @csrf
 
                 <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
@@ -74,7 +74,9 @@
 
                 <div class="lg:grid lg:grid-cols-2 flex flex-col gap-3">
 
-                    <img class="w-full aspect-square border rounded-lg auto" :src="inputProductImageSource" />
+                    <div class="w-full aspect-square border border-dashed border-gray-500 rounded-lg auto bg-clip-padding">
+                        <img class="rounded-lg" :src="inputProductImageSource" />
+                    </div>
 
                     <div class="flex flex-col grow">
                         <div class="mb-4">
@@ -91,13 +93,15 @@
                 </div>
 
                 <div class="mt-6 flex gap-3">
-                    <x-primary-button class='{cursor-not-allowed: !isUploadingProductImage}'>
+                    <button :class="{'cursor-not-allowed': isUploadingProductImage || inputProductImageSource === null}"
+                            :disabled="isUploadingProductImage || inputProductImageSource === null"
+                            class="inline-flex flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 disabled:opacity-25">
                         <svg :class="{hidden: !isUploadingProductImage}" class="animate-spin -ml-1 mr-3 h-3 w-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                         </svg>
                         {{ __('Save') }}
-                    </x-primary-button>
+                    </button>
 
                     <x-secondary-button class="ms-auto" x-on:click="$dispatch('close')">
                         {{ __('Cancel') }}
@@ -145,9 +149,16 @@
                             'x-csrf-token': '{{ csrf_token() }}'
                         }
                     }).then( response => {
-                        this.isUploadingProductImage = false;
+                        this.reset();
                         console.log(response);
                     });
+                },
+                reset() {
+                    this.$dispatch('close');
+                    this.inputProductImageSource = null;
+                    this.productImageName = null;
+                    this.isUploadingProductImage = false;
+                    this.$refs.productImageForm.reset();
                 }
             }
         }
