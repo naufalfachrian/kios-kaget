4a0aacdc45497fb61a8dc597853c82996dcbe26a
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-11-28 11:36:32 +0700

    Move notifications to template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,13 +32,26 @@
                 {{ $slot }}
             </main>
 
-            @if (isset($notifications))
-                <div class="fixed right-0 bottom-0 m-6 gap-3 flex flex-col" x-data="{
+            <div class="fixed right-0 bottom-0 m-6 gap-3 flex flex-col" x-data="{
                     notifications: [],
                 }" x-on:push-notification.window="notifications.push($event.detail)">
-                    {{ $notifications }}
-                </div>
-            @endif
+                <template x-for="notification in notifications">
+                    <div class="rounded-lg p-4 shadow-xl backdrop-blur-xl transform transition-all" :class="notification.color"
+                         x-data="{show: false}"
+                         x-show="show"
+                         x-init="$nextTick(() => { show = true });setTimeout(() => show = false, 5000);"
+                         x-transition:enter="ease-out duration-300"
+                         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                         x-transition:leave="ease-in duration-200"
+                         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                        <span class="font-semibold text-sm text-white" x-text="notification.title"></span><br/>
+                        <span class="text-sm text-white" x-text="notification.text"></span><br/>
+                    </div>
+                </template>
+            </div>
         </div>
     </body>
 </html>
--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -5,25 +5,6 @@
         </h2>
     </x-slot>
 
-    <x-slot name="notifications">
-        <template x-for="notification in notifications">
-            <div class="rounded-lg p-4 shadow-xl backdrop-blur-xl transform transition-all" :class="notification.color"
-                 x-data="{show: false}"
-                 x-show="show"
-                 x-init="$nextTick(() => { show = true });setTimeout(() => show = false, 5000);"
-                 x-transition:enter="ease-out duration-300"
-                 x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                 x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-                 x-transition:leave="ease-in duration-200"
-                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                 x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-                <span class="font-semibold text-sm text-white" x-text="notification.title"></span><br/>
-                <span class="text-sm text-white" x-text="notification.text"></span><br/>
-            </div>
-        </template>
-    </x-slot>
-
     <div class="py-12" x-data="productForm()">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
