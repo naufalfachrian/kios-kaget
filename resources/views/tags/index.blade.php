b596c7ae0f684ceb4fd20673168375b7c8a81732
bungamungil <bungamungil@icloud.com>
2023-11-30 20:41:31 +0700

    Fix reset form tag group

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -4,7 +4,7 @@
             {{ __('Tags') }}
         </h2>
         <button class="btn--primary ms-auto" x-data=""
-                               x-on:click.prevent="$dispatch('open-modal', 'form-new-tag-group'); $dispatch('button-new-tag-group-click', '');">
+                               x-on:click="$dispatch('open-modal', 'form-new-tag-group'); $dispatch('button-new-tag-group-click', ''); $dispatch('reset-form-tag-group');">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
             </svg>
@@ -12,7 +12,9 @@
         </button>
     </x-slot>
 
-    <div class="py-12" x-data="tagIndex()" x-init="flashMessage()" x-on:button-new-tag-group-click.window="formNewTagGroupProps.action = '{{ route('tag-groups.store') }}'; formNewTagGroupProps.method = 'post'; formNewTagGroupProps.buttonText = '{{ __('Save') }}';">
+    <div class="py-12" x-data="tagIndex()" x-init="flashMessage()"
+         @button-new-tag-group-click.window="formNewTagGroupProps.action = '{{ route('tag-groups.store') }}'; formNewTagGroupProps.method = 'post'; formNewTagGroupProps.buttonText = '{{ __('Save') }}';"
+         @reset-form-tag-group.window="resetSelectedTagGroup()">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 gap-4 flex flex-col">
             @if (count($tagGroups) == 0)
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
@@ -191,6 +193,13 @@
                 resetSelectedTag() {
                     this.selectedTag = {
                         tag_group_id: null,
+                        name: null,
+                        id: null,
+                    }
+                },
+                resetSelectedTagGroup() {
+                    console.log('reset')
+                    this.selectedTagGroup = {
                         name: null,
                         id: null,
                     }
