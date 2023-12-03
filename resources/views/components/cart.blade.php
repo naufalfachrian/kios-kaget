c4c996ea27128d2804fc6c33e087ef50a11eaa76
bungamungil <bungamungil@icloud.com>
2023-12-03 20:39:41 +0700

    Set empty cart view

--- resources/views/components/cart.blade.php
+++ resources/views/components/cart.blade.php
@@ -3,6 +3,11 @@
     <div class="p-6 border-b border-blue-200">
         <span class="text-xl font-semibold">{{ __('Cart') }}</span>
     </div>
+    <template x-if="subTotal === 0">
+        <div class="p-6 flex flex-col gap-3">
+            <span>{{ __('Your cart is empty. Please shop!') }}</span>
+        </div>
+    </template>
     <div class="grow h-auto overflow-y-scroll p-6 flex flex-col gap-6">
         <template x-for="cartDetail in cartDetails">
             <div class="flex flex-row gap-2 rounded-xl border border-orange-200 p-4">
@@ -44,28 +49,30 @@
             </div>
         </template>
     </div>
-    <div class="p-6 border-t border-blue-200 flex flex-col gap-3">
-        <div class="flex-row flex">
-            <span class="text-xl font-semibold">{{ __('Sub Total') }}</span>
-            <span class="text-xl font-semibold ms-auto" x-text="new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumFractionDigits: 0 }).format(subTotal,)"></span>
+    <template x-if="subTotal > 0">
+        <div class="p-6 border-t border-blue-200 flex flex-col gap-3">
+            <div class="flex-row flex">
+                <span class="text-xl font-semibold">{{ __('Sub Total') }}</span>
+                <span class="text-xl font-semibold ms-auto" x-text="new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumFractionDigits: 0 }).format(subTotal,)"></span>
+            </div>
+            <button class="btn--primary w-full items-center justify-center"
+                    :class="{'cursor-not-allowed': isSubmitting}"
+                    :disabled="isSubmitting">
+                <svg :hidden="isSubmitting" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                </svg>
+                <svg :hidden="!isSubmitting"
+                     class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                            stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                {{ __('Checkout') }}
+            </button>
         </div>
-        <button class="btn--primary w-full items-center justify-center"
-                :class="{'cursor-not-allowed': isSubmitting}"
-                :disabled="isSubmitting">
-            <svg :hidden="isSubmitting" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
-            </svg>
-            <svg :hidden="!isSubmitting"
-                 class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
-                 fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                        stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-            {{ __('Checkout') }}
-        </button>
-    </div>
+    </template>
 </div>
 
 <script>
