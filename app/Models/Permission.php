1e13e2ae816ccba57881ae54073f52b37168ea64
bungamungil <bungamungil@icloud.com>
2023-12-10 19:20:36 +0700

    Fix Permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Permission extends Model
 {
-    use HasFactory;
+    use HasUuids, HasFactory;
 
     static $PRODUCT_MASTER = "PRODUCT_MASTER";
 
