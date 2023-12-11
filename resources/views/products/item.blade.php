5564c4c594ed51e910ea8023760ea41cf1502e2a
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-12-11 17:29:56 +0700

    Polished product item

--- resources/views/products/item.blade.php
+++ resources/views/products/item.blade.php
@@ -19,20 +19,35 @@
                             @endif
                         @endif
                     </div>
-                    <div class="bg-white shadow-md rounded-lg md:basis-1/2 p-6 gap-4 flex flex-col">
-                        <h2 class="font-semibold text-3xl text-gray-800 leading-tight">
+                    <div class="bg-white shadow-md rounded-lg md:basis-1/2 p-6 flex flex-col">
+                        @if (isset($product->category))
+                        <div class="flex flex-row items-center mb-4">
+                            <span class="text-xs p-1 rounded-md whitespace-nowrap">{{ $product->category->group->name }}</span>
+                            <x-heroicons-chevron-right class="w-3 h-3"></x-heroicons-chevron-right>
+                            <span class="text-xs p-1 rounded-md whitespace-nowrap">
+                                <a href="{{ route('categories.show', ['category' => $product->category]) }}">
+                                    {{ $product->category->name }}
+                                </a>
+                            </span>
+                        </div>
+                        @endif
+                        @if (count($product->tags) > 0)
+                        <div class="flex flex-wrap justify-start gap-1">
+                            @foreach ($product->tags as $tag)
+                                <span class="bg-brand-yellow text-brand-light text-xs p-1 rounded-md whitespace-nowrap">
+                                    <a href="{{ route('tags.show', ['tag' => $tag]) }}">
+                                        {{ $tag->name }}
+                                    </a>
+                                </span>
+                            @endforeach
+                        </div>
+                        @endif
+                        <h2 class="font-semibold text-3xl text-gray-800 leading-tight mb-4">
                             {{ $product->name }}
                         </h2>
-                        @if (count($product->tags) > 0)
-                            <div class="flex flex-wrap justify-start gap-1">
-                                @foreach ($product->tags as $tag)
-                                    <span class="bg-green-100 text-xs p-1 rounded-md whitespace-nowrap">{{ $tag->name }}</span>
-                                @endforeach
-                            </div>
-                        @endif
-                        <p class="font-bold text-3xl text-gray-800">Rp {{ number_format($product->price, 0, ',', '.') }}</p>
+                        <p class="font-bold text-2xl text-gray-800">Rp {{ number_format($product->price, 0, ',', '.') }}</p>
                         <p class="font-normal text-md text-gray-600">{{ $product->description }}</p>
-                        <hr class="border-gray-500"/>
+                        <hr class="border-gray-200 my-6"/>
                         <x-add-to-cart :product="$product" class=""></x-add-to-cart>
                     </div>
                 </div>
