4070c666aabfddde55ee4994e4f8fc480156e7a4
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-12-11 14:04:05 +0700

    Update tag product listing

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 gap-4 flex flex-col">
             <div class="flex flex-col gap-2">
-                <x-product-listing title="Tag: {{ $tag->name }}" :products="$products"></x-product-listing>
+                <x-product-listing title="{{ $tag->group->name }}: {{ $tag->name }}" :products="$products"></x-product-listing>
             </div>
         </div>
     </div>
